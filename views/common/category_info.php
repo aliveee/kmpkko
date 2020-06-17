d557be5b9aecabfc516263247b27109db615b751
kozhevnikovpe <kozhevnikov-kovrov@yandex.ru>
2020-06-17 22:23:16 +0300

    #310

--- views/common/category_info.php
+++ views/common/category_info.php
@@ -1,6 +1,19 @@
 <div class="category-info">
     <div class="container bg-light-grey p-32">
         <div class="row">
+            <div class="col">
+                <img class="category-info__img" src="/uploads/catalog/320x-/<?=$catalog["id"]?>_info.jpg"/>
+                <h1 class="h"><?=@$page["h1"]?></h1>
+                <?=$catalog["text"]?>
+                <!--<a href="#request_form" class="mt-32 btn btn-primary">Отправить запрос</a>-->
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<? /*
+ <div class="row">
             <div class="col-12 col-lg-4">
                 <div class="row mb-lg-0 mb-3">
                     <div class="col-4 col-lg">
@@ -14,8 +27,7 @@
             <div class="col-12 col-lg-8">
                 <h1 class="h d-none d-lg-block"><?=@$page["h1"]?></h1>
                 <?=$catalog["text"]?>
-                <a href="#request_form" class="mt-32 btn btn-primary">Отправить запрос</a>
+                <!--<a href="#request_form" class="mt-32 btn btn-primary">Отправить запрос</a>-->
             </div>
         </div>
-    </div>
-</div>
+ */ ?>
